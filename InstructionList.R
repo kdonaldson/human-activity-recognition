7f6f07ab460244d498ef40162bd5b13f4d86e062
Keith Donaldson <keith.donaldson@kdonaldsonllc.com>
2015-04-24 15:11:39 -0400

    created tidy observations table

--- InstructionList.R
+++ InstructionList.R
@@ -9,6 +9,9 @@
 #Step 2 - run the software separately for each sample
 #Step 3 - take column three of outputfile.out for each sample and that is the corresponding row in the output data set
 
+library(dplyr)
+library(reshape2)
+
 #this.dir <- dirname(parent.frame(2)$ofile)
 #setwd(this.dir)
 this.dir <- "/Users/kdonaldson/Projects/GettingCleaningData/Project/human-activity-recognition"
@@ -16,6 +19,7 @@ this.dir <- "/Users/kdonaldson/Projects/GettingCleaningData/Project/human-activi
 setwd(paste(this.dir, "/UCI HAR Dataset/", sep = ""))
 activities = read.table("activity_labels.txt")
 features = read.table("features.txt")
+features$V2 = make.names(features$V2, unique = TRUE)
 
 setwd(paste(this.dir, "/UCI HAR Dataset/test/", sep = ""))
 subjects_test = read.table("subject_test.txt")
@@ -47,23 +51,21 @@ rm(file)
 rm(file_name)
 rm(files)
 
-
-# library("dplyr")
-library(plyr)
-
 # Rename subjects vectors
-subjects_train = rename(subjects_train, c("V1"="subject"))
-subjects_test = rename(subjects_test, c("V1"="subject"))
+subjects_train = rename(subjects_train, subject=V1)
+subjects_test = rename(subjects_test, subject=V1)
 
 # Rename activity vectors
-activities_train = rename(activities_train, c("V1"="activity"))
-activities_test = rename(activities_test, c("V1"="activity"))
+activities_train = rename(activities_train, activity=V1)
+activities_test = rename(activities_test, activity=V1)
 
 # Rename features columns
 colnames(features_train) <- features[,2]
 colnames(features_test) <- features[,2]
 
+# Uses descriptive activity names to name the activities in the data set
 # Recode activities based on lookup table
+activities$V2 = as.character(activities$V2)
 for(i in seq_len(nrow(activities))){  #Make replacements
   col.to <- activities$V2[i] 
   col.from <- activities$V1[i]
@@ -71,5 +73,61 @@ for(i in seq_len(nrow(activities))){  #Make replacements
   activities_test$activity <- ifelse(activities_test$activity == col.from, col.to, activities_test$activity)
 }
 
-# Merge data frames
-train = cbind(subjects_train, activities_train, features_train)
+train = cbind(subjects_train,activities_train, features_train)
+test = cbind(subjects_test,activities_test, features_test)
+
+# Merge the training and the test sets to create one data set.
+observations = rbind(train, test)
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+observations = select(.data = observations, subject, activity, matches("(mean|std)"))
+
+body_acc_x = rbind(body_acc_x_train, body_acc_x_test)
+observations_body_acc_x = cbind(observations, body_acc_x, row.names = NULL)
+observations_body_acc_x_melt <- melt(observations_body_acc_x, id=1:88)
+
+body_acc_y = rbind(body_acc_y_train, body_acc_y_test)
+observations_body_acc_y = cbind(observations, body_acc_y, row.names = NULL)
+observations_body_acc_y_melt <- melt(observations_body_acc_y, id=1:88)
+
+body_acc_z = rbind(body_acc_z_train, body_acc_z_test)
+observations_body_acc_z = cbind(observations, body_acc_z, row.names = NULL)
+observations_body_acc_z_melt <- melt(observations_body_acc_z, id=1:88)
+
+body_gyro_x = rbind(body_gyro_x_train, body_gyro_x_test)
+observations_body_gyro_x = cbind(observations, body_gyro_x, row.names = NULL)
+observations_body_gyro_x_melt <- melt(observations_body_gyro_x, id=1:88)
+
+body_gyro_y = rbind(body_gyro_y_train, body_gyro_y_test)
+observations_body_gyro_y = cbind(observations, body_gyro_x, row.names = NULL)
+observations_body_gyro_y_melt <- melt(observations_body_gyro_y, id=1:88)
+
+body_gyro_z = rbind(body_gyro_z_train, body_gyro_z_test)
+observations_body_gyro_z = cbind(observations, body_gyro_z, row.names = NULL)
+observations_body_gyro_z_melt <- melt(observations_body_gyro_z, id=1:88)
+
+total_acc_x = rbind(total_acc_x_train, total_acc_x_test)
+observations_total_acc_x = cbind(observations, total_acc_x, row.names = NULL)
+observations_total_acc_x_melt <- melt(observations_total_acc_x, id=1:88)
+
+total_acc_y = rbind(total_acc_y_train, total_acc_y_test)
+observations_total_acc_y = cbind(observations, total_acc_y, row.names = NULL)
+observations_total_acc_y_melt <- melt(observations_total_acc_y, id=1:88)
+
+total_acc_z = rbind(total_acc_z_train, total_acc_z_test)
+observations_total_acc_z = cbind(observations, total_acc_z, row.names = NULL)
+observations_total_acc_z_melt <- melt(observations_total_acc_z, id=1:88)
+
+observations = cbind(
+  observations, 
+  body_acc_x = observations_body_acc_x_melt$value, 
+  body_acc_y = observations_body_acc_y_melt$value, 
+  body_acc_z = observations_body_acc_z_melt$value, 
+  body_gyro_x = observations_body_gyro_x_melt$value,
+  body_gyro_y = observations_body_gyro_y_melt$value,
+  body_gyro_z = observations_body_gyro_z_melt$value, 
+  total_acc_x = observations_total_acc_x_melt$value, 
+  total_acc_y = observations_total_acc_y_melt$value, 
+  total_acc_z = observations_total_acc_z_melt$value, 
+  row.names = NULL 
+  )
